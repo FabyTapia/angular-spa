884a10d0552450ea3e99100a3ebbc1d1eeed3767
FabyTapia <ftapiamorales@gmail.com>
2019-06-16 22:44:54 -0400

    se utiliza output para emitir evento de hijo a padre

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { Router } from '@angular/router';
-
+//output emiter evento de hijo a padre va dela mano con eventemitter elemento
 //input le dice angular propiedad recibida desde fuera 
 @Component({
   selector: 'app-heroe-tarjeta',
@@ -12,15 +12,22 @@ export class HeroeTarjetaComponent implements OnInit {
     @Input() heroe: any = {};//propiedad normal se agrefa decorador input
     @Input() index: number;//propiedad recibida desde afuera de componente 
   
-  
-  
-    constructor(private router: Router) { }
+  @Output() heroeSeleccionado: EventEmitter<number>;
+            //observable
+
+  //propiedad que el padre nesecita escuchar 
+    constructor(private router: Router) { 
+      this.heroeSeleccionado = new EventEmitter(); //aqui se inicializa eventemiter
+    }
 
   ngOnInit() {
 
   }
 verHeroe(){
-console.log( this.index);
-this.router.navigate(['/heroe', this.index]);
+//console.log( this.index);
+//this.router.navigate(['/heroe', this.index]);
+this.heroeSeleccionado.emit( this.index);
+
 }
+
 }
